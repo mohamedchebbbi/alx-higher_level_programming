3be8a5952555237794074f38eb367de4cf94f93e
mohamed <mohamedchebbbi@gmail.comgit config --global user.name mohamed>
2022-06-02 12:00:32 -0700

    hot fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    l = len(sys.argv) - 1
-    print("{} argument{}{}"
-          .format(l, "" if l == 1 else "s", "." if l == 0 else ":"))
-    for i in range(l):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    from sys import argv
+    arc = len(argv)
+    if arc > 2:
+        print("{:d} {:s}:".format(arc-1, "arguments"))
+        for i, s in enumerate(argv[1:]):
+            print("{:d}: {:s}".format(i+1, s))
+    if arc == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    if arc == 1:
+        print("0 arguments.")
