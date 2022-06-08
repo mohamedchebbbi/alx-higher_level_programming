70a6b4ddab5cbdd055b816307e96dccc8f4a2ac9
mohamed <mohamedchebbbi@gmail.comgit config --global user.name mohamed>
2022-06-08 14:54:56 -0700

    fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if not a_dictionary:
         return None
-    maxval = 0
-    maxkey = None
-    for k, v in a_dictionary.items():
-        if v > maxval:
-            maxval = v
-            maxkey = k
-    return maxkey
+    max_value = max(a_dictionary.values())
+    for key in a_dictionary.keys():
+        if a_dictionary[key] == max_value:
+            return key
