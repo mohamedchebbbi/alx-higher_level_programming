a58bba108e093ef7279adb51170481d552da7865
mohamedchebbbi <mohamedchebbbi@gmail.com>
2023-06-05 17:23:07 -0700

    fixing requirement fail

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -2,8 +2,8 @@
 
 class Rectangle:
     pass
-
-my_rectangle = Rectangle()
-print(type(my_rectangle))
-print(my_rectangle.__dict__)
+if __name__ == "__main__":
+ my_rectangle = Rectangle()
+ print(type(my_rectangle))
+ print(my_rectangle.__dict__)
 
