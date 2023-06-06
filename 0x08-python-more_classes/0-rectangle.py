cb20907fd936007dc0ea6acbac9fbfb86b16b537
mohamedchebbbi <mohamedchebbbi@gmail.com>
2023-06-05 17:34:39 -0700

    solved 0-rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -3,11 +3,7 @@
 class Rectangle:
     pass
 
-if __name__ == "__main__":
-    my_rectangle = Rectangle()
-    class_name = type(my_rectangle).__name__
-    file_name = __file__.split("/")[-1].split(".")[0]
-    print(f"<class '{file_name}.{class_name}'>")
-    print("{}")
-
+my_rectangle = Rectangle()
+print(type(my_rectangle))
+print(my_rectangle.__dict__)
 
