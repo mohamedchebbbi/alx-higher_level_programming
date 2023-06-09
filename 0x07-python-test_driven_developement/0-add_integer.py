844bf21322ba8c9adb5fc3745e89f3eafc26b307
mohamedchebbbi <mohamedchebbbi@gmail.com>
2023-06-08 17:18:02 -0700

    tasks

---
+++ 0x07-python-test_driven_developement/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""add_integer method"""
+
+
+def add_integer(a, b=98):
+    """Adds two integers
+
+    Args:
+        a: the first integer.
+        b: the second integer, default 98.
+
+    Raises:
+        TypeError: if a, b are not int, float
+
+    Returns:
+        The sum of a and b
+    """
+
+    if type(a) not in (int, float):
+        raise TypeError('a must be an integer')
+    if type(b) not in (int, float):
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
