23525e94600e0d27b75d549572be752aa9671fb0
mohamedchebbbi <mohamedchebbbi@gmail.com>
2023-06-12 16:57:43 -0700

    task 4 to 8

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+3!/usr/bin/python3
+"""check if obj is instance class or inherited class"""
+
+def is_kind_of_class(obj, a):
+    """return true if instance else false"""
+    
+    return (instance(obj, a)
