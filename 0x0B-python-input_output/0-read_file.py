1a15c37a0cd6470a45525b60e73f04296cfb38fa
mohamedchebbbi <mohamedchebbbi@gmail.com>
2023-06-13 15:03:04 -0700

    adding 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ MODULE FOR READ8FILE"""
 
-def read_file(filenme=""):
+
+def read_file(filename=""):
     """METHOD FOR READ FROM FILE"""
     with open(filename, "r", encoding="utf-8") as f:
         text = f.read()
