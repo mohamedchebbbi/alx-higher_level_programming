b6e36f30f9de9f229614f4015f0953165ddcd981
mohamedchebbbi <mohamedchebbbi@gmail.com>
2023-07-18 16:18:09 -0700

    ez peasy

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
--- Displays the number of records with id = 89
-SELECT count(*) FROM first_table WHERE 'id' = 89;
+-- Displays number of records with id of 89
+SELECT count(*) FROM first_table WHERE `id` = 89;
