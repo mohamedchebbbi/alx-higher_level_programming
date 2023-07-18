e3c43014574c6c921edafe4e1202a29f17adc358
mohamedchebbbi <mohamedchebbbi@gmail.com>
2023-07-18 09:25:08 -0700

    ez peasy

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- Insert new row in table "first_table" (database "hbtn_0c_0)
-SELECT INTO first_table(id,name) VALUES (89, "Best School");
+-- Insert new row in table.
+SELECT INTO first_table(id,name) VALUES (89, 'Best School');
