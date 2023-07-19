c66bfa456acfe85bd4e9f2ba78e2180957bc0baf
mohamedchebbbi <mohamedchebbbi@gmail.com>
2023-07-18 17:00:06 -0700

    ez peasy

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,9 @@
---  converts database to UTF8
-ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert the database to UTF-8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the table to UTF-8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the field in the table to UTF-8
 ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
