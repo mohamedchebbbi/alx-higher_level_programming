54838fdebf059ed4a1d71032a66fc8c1afde4d37
mohamedchebbbi <mohamedchebbbi@gmail.com>
2023-07-20 01:05:03 +0100

    ez peasy

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Creates the user.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
+
---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- Creates the table force_name on MySQL server.
+CREATE TABLE IF NOT EXISTS `force_name` (
+    `id`   INT,
+    `name` VARCHAR(256) NOT NULL
+);
