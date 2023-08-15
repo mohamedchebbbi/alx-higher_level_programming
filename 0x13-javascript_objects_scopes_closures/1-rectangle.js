5b38e2f79e60f64e22aa125743368834c80fdfb7
mohamed chebbi <mohamedchebbbi@gmail.com>
2023-08-15 15:02:57 +0000

    ez peazy

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor (a, b) {
-    this.with = a;
-    this.height = b;
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
   }
 };
