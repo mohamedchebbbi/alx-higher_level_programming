7d8bda9c9b586d9bc48f5d15153d6775de82a75c
mohamed chebbi <mohamedchebbbi@gmail.com>
2023-08-20 14:25:18 -0700

    ez peazy

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' list all states where name matche argumentfrom database '''
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+            user=argv[1], passwd=argv[2],
+            db=argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE BINARY name = %s ORDER BY id", (argv[4]))
+    db.close()
+    rows = c.fetchall()
+    for row in rows:
+        print("{}".format(row))
