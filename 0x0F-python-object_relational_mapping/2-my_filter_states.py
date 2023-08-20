c8b069cc3c21b3c173331fda3cf4a3956f1311dd
mohamed chebbi <mohamedchebbbi@gmail.com>
2023-08-20 14:45:58 -0700

    ez peazy

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,12 +5,15 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    db = MySQLdb.connect(host="localhost", port=3306,
-            user=argv[1], passwd=argv[2],
-            db=argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM states WHERE BINARY name = %s ORDER BY id", (argv[4],))
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s ORDER BY id",
+                (argv[4],))
     db.close()
-    rows = c.fetchall()
+    rows = cur.fetchall()
     for row in rows:
         print("{}".format(row))
