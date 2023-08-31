127d492c828f9fbc751b4140d3195470ee8aac20
mohamed chebbi <mohamedchebbbi@gmail.com>
2023-08-31 16:43:06 -0700

    fix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept
-
 curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-
 curl -sH "X-School-User-Id: 98" "$1"
