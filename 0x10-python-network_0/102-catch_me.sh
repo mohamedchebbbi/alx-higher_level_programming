ac25bd8ec9ca321cb04dfe420eea3ff1bba88a40
mohamed chebbi <mohamedchebbbi@gmail.com>
2023-08-31 16:58:25 -0700

    advanced

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
+
+curl -sLX PUT -H "origin: BestSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
