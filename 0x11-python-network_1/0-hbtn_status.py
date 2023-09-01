c496c5e186f06cfbb03396c58addbfa635cd4e50
mohamed chebbi <mohamedchebbbi@gmail.com>
2023-09-01 14:59:00 -0700

    ez peazy

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
-        bodey = responsee.read()
+        body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
