1db9253a22ef8ad047bb8f765d5a292006c0b166
mohamed chebbi <mohamedchebbbi@gmail.com>
2023-08-31 17:11:24 -0700

    fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
 """ Python script finds a peak in a list of unsorted integers. """
 
-def find_peak(l):
+def find_peak(list_i):
     """
-    args: l : list of integers to find peak
+    args: 
+        list_i : list of integers to find peak
     Returns: peak of l
     """
-    s = len(l)
+    s = len(list_i)
     m_e = s
     m = s // 2
-
     if s == 0:
         return None
-
     while True:
         m_e = m_e // 2
-        if (m < s - 1 and l[m] < l[m + 1]):
+        if (m < s - 1 and list_i[m] < list_i[m + 1]):
             if m_e // 2 == 0:
                 m_e = 2
             m = m + m_e // 2
-        elif m_e > 0 and l[m] < l[m - 1]:
+        elif m_e > 0 and list_i[m] < list_i[m - 1]:
             if m_e // 2 == 0:
                 m_e = 2
             m = m - m_e // 2
         else:
-            return l[m]
+            return list_i[m]
