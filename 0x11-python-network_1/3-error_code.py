590bbad8bef4fd7e1ad70a81d56806e16bcb92bc
mohamed chebbi <mohamedchebbbi@gmail.com>
2023-09-01 15:22:59 -0700

    3. Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+3. Error code #0
+"""
+import urllib.error
+import urrlib.request
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
