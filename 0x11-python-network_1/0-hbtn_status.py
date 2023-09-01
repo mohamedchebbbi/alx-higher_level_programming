3c93b5cd87bc83bf8acd3d01fc241b5cd17244fe
mohamed chebbi <mohamedchebbbi@gmail.com>
2023-09-01 14:57:08 -0700

    fix

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" fetches https://alx-intranet.hbtn.io/status """
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        bodey = responsee.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-0x11-python-network_1
+alx higher lvl prog
